305cde84b82477d65a62ba81da49787299b9764f
Kingsley31 <kingsleyemeka31@gmail.com>
2025-07-16 00:51:14 +0100

    Completed meter consumption chart

--- features/meter/api/meter-consumption-chart.api.ts
+++ features/meter/api/meter-consumption-chart.api.ts
@@ -1,34 +1,16 @@
+import { apiClient } from "@/lib/http-client";
 import { MeterConsumptionChartData } from "../meter.types";
 
 export type MeterConsumptionChartFilter = {
-    numberOfPastMonths?: number;
+    numberOfPastMonths: number;
     meterId: string;
 }
 
-export async function getMeterConsumptionChartData(params: MeterConsumptionChartFilter): Promise<MeterConsumptionChartData[]> {
-    const fullData: MeterConsumptionChartData[] = [
-      { month: "Aug 2024", consumption: 100 },
-      { month: "Sep 2024", consumption: 115 },
-      { month: "Oct 2024", consumption: 130 },
-      { month: "Nov 2024", consumption: 120 },
-      { month: "Dec 2024", consumption: 140 },
-      { month: "Jan 2025", consumption: 125 },
-      { month: "Feb 2025", consumption: 110 },
-      { month: "Mar 2025", consumption: 120 },
-      { month: "Apr 2025", consumption: 135 },
-      { month: "May 2025", consumption: 110 },
-      { month: "Jun 2025", consumption: 145 },
-      { month: "Jul 2025", consumption: 160 },
-    ];
-    // throw new Error('Netework Error');
-    if (params.numberOfPastMonths) {
-        const filterOptions: Record<string,MeterConsumptionChartData[]> = {
-                "3": fullData.slice(-3),
-                "5": fullData.slice(-5),
-                "12": fullData, // past year
-            }
-        const data = filterOptions[params.numberOfPastMonths.toString()];
-        return data;
+export async function getMeterConsumptionChartData(data: MeterConsumptionChartFilter): Promise<MeterConsumptionChartData[]> {
+    const params = {
+        numberOfPastMonths: data.numberOfPastMonths,
     }
-    return fullData;
+    const response = await apiClient.get<MeterConsumptionChartData[]>(`/meters/${data.meterId}/consumption-chart`,{params,});
+        console.log(response.data);
+    return response.data;
 }
